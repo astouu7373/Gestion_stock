ef7b2bf890956ba16fa1ab353f5ccaf88597d363
astou7373 <assetouditeastou_diawara@reseau.eseo.fr>
2025-05-28 14:58:19 +0200

    test

--- test/model/ProductTest.java
+++ test/model/ProductTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductTest {
  @Test
 void testConstructeur(){
-     Product product = new ElectrocicProduct("Chargeur", 20, 20);
+     Product product = new ElectrocicProduct("Chargeur", 20, 20, 3);
      assertEquals("Chargeur", product.getNom());
      assertEquals(20, product.getQuantité());
      assertEquals(20,product.getPrix());
