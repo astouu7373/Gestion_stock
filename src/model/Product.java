eb0b33584abf475322053ea0b68517fcd74cad5d
astou7373 <assetouditeastou_diawara@reseau.eseo.fr>
2025-05-28 16:07:51 +0200

    test

--- src/model/Product.java
+++ src/model/Product.java
@@ -19,6 +19,7 @@ public abstract class Product {
     public void setQuantité(int Quantité){
         this.quantité = quantité;
     }
+
     public double getPrix(){return prix;};
     public void SetPrix( double prix){
         this.prix = prix;
