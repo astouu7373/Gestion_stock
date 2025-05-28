8454a439dcc2fd903851d2549561c06bb4828370
astou7373 <assetouditeastou_diawara@reseau.eseo.fr>
2025-05-28 14:51:30 +0200

    test

--- src/model/ElectrocicProduct.java
+++ src/model/ElectrocicProduct.java
@@ -1,4 +1,10 @@
 package model;
 
-public class ElectrocicProduct {
+public class ElectrocicProduct extends  Product{
+    private String nom;
+    private double garantie;
+
+    ElectrocicProduct(String nom, int quantité, double prix) {
+        super(nom, quantité, prix);
+    }
 }
